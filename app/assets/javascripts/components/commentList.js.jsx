eeb933f3748dd1e4fad69eb02b9c048384eb9065
Ryan <ryanpauley@gmail.com>
2015-06-29 09:03:34 +1000

    possibly bricked files

--- app/assets/javascripts/components/commentList.js.jsx
+++ app/assets/javascripts/components/commentList.js.jsx
@@ -1,12 +1,14 @@
 var CommentList = React.createClass({
   render: function() {
-    var commentNodes=this.props.data.map(function (comment) {
+    if (this.props.data) {
+      var commentNodes = this.props.data.map(function (comment) {
       return (
         <Comment author={comment.author}>
           {comment.text}
         </Comment>
         );
-    });
+      });
+    }
     return (
       <div className="commentList">
         {commentNodes}
